b13cfa4295bffcfaf92b5df6656b0dc7180d2c3a
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2025-11-15 21:35:50 +0300

    Set dock to fixed

--- fedintosher.sh
+++ fedintosher.sh
@@ -25,6 +25,7 @@ echo "2.2 - Applying Dash to Dock settings"
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
 dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink true
 dconf write /org/gnome/shell/extensions/dash-to-dock/click-action "'minimize'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
 gext install arcmenu@arcmenu.com
 echo "2.3 - Applying ArcMenu settings"
 gext install user-theme@gnome-shell-extensions.gcampax.github.com
