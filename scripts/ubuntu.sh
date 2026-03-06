962929d73b7a463b55149104ba2e84bd0c024950
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2026-03-06 22:11:32 +0300

    ubuntu sucks omg

---
+++ scripts/ubuntu.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+cd ~
+mkdir ".fedintosh"
+echo "1 - Installing GEXT and git."
+sudo apt install python3-pip git -y
+pip install gnome-extensions-cli --break-system-packages
+echo "Temporarily modifying PATH..."
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "2 - Installing extensions."
+echo "Disabling Ubuntu extensions..."
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
+echo "Accept all the dialog popups you see!"
+gext install blur-my-shell@aunetx
+echo "2.1 - Applying Blur My Shell settings"
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/blur true
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/dynamic-opacity false
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/enable-all true
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/opacity 230
+gext install dash-to-dock@micxgx.gmail.com
+echo "2.2 - Applying Dash to Dock settings"
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink true
+dconf write /org/gnome/shell/extensions/dash-to-dock/click-action "'minimize'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
+dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 36
+dconf write /org/gnome/shell/extensions/dash-to-dock/hot-keys false
+gext install arcmenu@arcmenu.com
+echo "2.3 - Applying ArcMenu settings"
+gext install user-theme@gnome-shell-extensions.gcampax.github.com
+echo "2.4 - Applying user theme"
+echo "Calling the themer, other extensions will continue later!"
+cd ~
+cd .fedintosh
+git clone https://github.com/vinceliuice/MacTahoe-gtk-theme
+cd "MacTahoe-gtk-theme"
+chmod +x install.sh
+./install.sh -b -l -c dark
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+sudo ./tweaks.sh -g
+sudo flatpak override --filesystem=xdg-config/gtk-3.0 && sudo flatpak override --filesystem=xdg-config/gtk-4.0
+gext install rounded-window-corners@fxgn
+echo "2.5 - Enabling titlebar buttons and changing the theme..."
+dconf write /org/gnome/desktop/wm/preferences/button-layout "'appmenu:minimize,maximize,close'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'MacTahoe-Dark'"
+dconf write /org/gnome/shell/extensions/user-theme/name "'MacTahoe-Dark'"
+echo "2.6 - Installing the icon theme..."
+cd ~
+cd .fedintosh
+git clone https://github.com/vinceliuice/MacTahoe-icon-theme
+cd MacTahoe-icon-theme
+./install.sh
+dconf write /org/gnome/desktop/interface/icon-theme "'MacTahoe'"
+echo "2.7 - Setting up the wallpaper..."
+cd ~/.fedintosh/MacTahoe-gtk-theme/wallpaper
+./install-gnome-backgrounds.sh
+dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/gnome/glass-chip-l.jxl'"
+dconf write /org/gnome/desktop/background/picture-uri-dark "'file:///usr/share/backgrounds/gnome/glass-chip-d.jxl'"
+
+echo "3 - Compiling a custom version of gnome-shell"
+# sudo apt install -y build-essential autoconf automake libtool pkg-config git meson ninja-build python3-pip gtk-doc-tools gnome-common sassc gjs libglib2.0-dev makepkg
+# cd ~/.fedintosh
+# mkdir "customshell"
+# cd customshell
+# echo "Fetching dependency gcr-4..."
+# wget http://ftp.es.debian.org/debian/pool/main/g/gcr4/libgcr-4-dev_4.4.0.1-8_amd64.deb
+# git clone https://gitlab.gnome.org/GNOME/gnome-shell
+# cd gnome-shell
+# echo "Installing deps..."
+# sudo apt install libatk-bridge2.0-dev libecal2.0-dev libedataserver1.2-dev libical-dev
+# meson setup _build
+# meson compile -C _build
+# cd src/gnome-shell
+# wget https://raw.githubusercontent.com/ios7jbpro/fedintosh/refs/heads/main/Shell_BlurEffect__rounded_corners_mask.patch
+# patch -p1 < Shell_BlurEffect__rounded_corners_mask.patch
+# cd ~/.fedintosh/customshell/gnome-shell
+# cd ~/.fedintosh/customshell
+# git clone https://github.com/aunetx/blur-my-shell
+# cd blur-my-shell
+# wget https://raw.githubusercontent.com/ios7jbpro/fedintosh/refs/heads/main/Add_corner_radius_to_NativeDynamicBlurEffect.patch
+# wget https://github.com/user-attachments/files/25559743/bms_blur_effect_rounded_corners.patch
+# patch -p1 < bms_blur_effect_rounded_corners.patch
+# make install
+# dconf write /org/gnome/shell/extensions/blur-my-shell/applications/corner-radius 17
+echo "WARNING: Skipping this step as Ubuntu is too outdated to support this."
+echo "App blur will be turned off."
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/blur false
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/enable-all false
+
+
+
+echo "4 - Cleaning up!"
+cd ~
+rm -rf ~/.fedintosh
+
+echo "5 - Rebooting to apply changes..."
+systemctl reboot -i
